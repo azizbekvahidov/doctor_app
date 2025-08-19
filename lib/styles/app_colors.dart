015fc46b32ffcc5d7dd97a695b51d495e18ab523
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-20 00:49:36 +0500

    feat: app color was created

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -1,1 +1,8 @@
-class AppColors {}
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF3A00E5),
+      grey = Colors.grey,
+      red = Color(0xFFFF0000),
+      lightGray = Color(0xFFD9D9D9);
+}
