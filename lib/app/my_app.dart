08dff7adad20c35d1449b4ce64abe98ef9d8b015
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-20 13:59:05 +0500

    feat: myapp was updated

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,26 +1,32 @@
+import 'package:doctor_app/core/pages/pages.dart';
+import 'package:doctor_app/core/pages/routes.dart';
+import 'package:doctor_app/features/onboard/controller/onboard_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    final OnboardController onboardController = Get.put(OnboardController());
+    return ShadApp(
+      debugShowCheckedModeBanner: false,
+      theme: ShadThemeData(
+        brightness: Brightness.light,
+        colorScheme: const ShadZincColorScheme.light(),
       ),
-      home: const HomePage(),
+      darkTheme: ShadThemeData(
+        brightness: Brightness.dark,
+        colorScheme: const ShadZincColorScheme.dark(),
+      ),
+      home: GetMaterialApp(
+        locale: Locale(onboardController.selectedLang.value ?? "uz"),
+        fallbackLocale: const Locale("en"),
+        initialRoute: Routes.onboard,
+        getPages: AppPages.pages,
+      ),
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
