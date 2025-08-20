47b07859a1ade7a80495ee1ef3b62ebe3e6c5d5a
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-20 13:59:17 +0500

    feat: bindings

--- lib/core/bindings/app_bindings.dart
+++ lib/core/bindings/app_bindings.dart
@@ -1,6 +1,9 @@
+import 'package:doctor_app/features/onboard/controller/onboard_controller.dart';
 import 'package:get/get.dart';
 
 class AppBindings extends Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.lazyPut(() => OnboardController());
+  }
 }
