bdf6a93d5ec42075028d4b34fd93f6d6a0d2a7c1
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-22 22:00:53 +0500

    feat: contents were seperated

---
+++ lib/features/main/contents/home_content.dart
@@ -0,0 +1,50 @@
+import 'package:doctor_app/features/main/widgets/header.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/utils.dart';
+
+import '../widgets/active_card.dart';
+import '../widgets/announcement_list.dart';
+import '../widgets/new_card.dart';
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Header(),
+          Padding(
+            padding: const EdgeInsets.all(8.0).copyWith(top: 15),
+            child: Column(
+              children: [
+                AnnouncementList(
+                  title: "new_ann".tr,
+                  height: 260,
+                  child: ListView.separated(
+                    itemCount: 15,
+                    separatorBuilder: (_, __) => const SizedBox(width: 10),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, __) => const NewCard(),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                AnnouncementList(
+                  title: "active_ann".tr,
+                  height: 175,
+                  child: ListView.separated(
+                    itemCount: 15,
+                    separatorBuilder: (_, __) => const SizedBox(width: 10),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, __) => const ActiveCard(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/main/main_page.dart
+++ lib/features/main/main_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/active_card.dart';
-import 'widgets/announcement_list.dart';
-import 'widgets/new_card.dart';
+import 'contents/home_content.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -15,10 +13,14 @@ class _MainPageState extends State<MainPage> {
   int index = 0;
 
   final List<Widget> pages = [
-    const HomeContent(),
+    HomeContent(),
     const Center(child: Text("Search Page")),
     const Center(child: Text("Profile Page")),
   ];
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,49 +33,6 @@ class _MainPageState extends State<MainPage> {
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-        ],
-      ),
-    );
-  }
-}
-
-class HomeContent extends StatelessWidget {
-  const HomeContent({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Header(),
-          Padding(
-            padding: const EdgeInsets.all(8.0).copyWith(top: 15),
-            child: Column(
-              children: [
-                AnnouncementList(
-                  title: "New announcements",
-                  height: 260,
-                  child: ListView.separated(
-                    itemCount: 15,
-                    separatorBuilder: (_, __) => const SizedBox(width: 10),
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (_, __) => const NewCard(),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                AnnouncementList(
-                  title: "Active announcements",
-                  height: 175,
-                  child: ListView.separated(
-                    itemCount: 15,
-                    separatorBuilder: (_, __) => const SizedBox(width: 10),
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (_, __) => const ActiveCard(),
-                  ),
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
