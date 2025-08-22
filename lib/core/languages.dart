525f73ed749b7835134aca4f324f6d9c9e92e743
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-22 21:59:30 +0500

    ref: new value was added

--- lib/core/languages.dart
+++ lib/core/languages.dart
@@ -8,12 +8,18 @@ class AppTranslations extends Translations {
       'login': 'Войти',
       'logout': 'Выйти',
       'next': 'Далше',
+      'dismiss': 'Увольнять',
+      'new_ann': 'Новые объявления',
+      'active_ann': 'Активные объявления',
     },
     'uz_UZ': {
       'hello': 'Salom',
       'login': 'Kirish',
       'logout': 'Chiqish',
       'next': 'Davom etish',
+      'dismiss': 'Bekor qilish',
+      'new_ann': 'Yangi buyurtmalar',
+      'active_ann': 'Aktiv buyurtmalar',
     },
   };
 }
