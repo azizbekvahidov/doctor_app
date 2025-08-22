d9097ce38995ba47461290fff09945097ff4f577
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-22 21:58:44 +0500

    ref: new page was added old page was removed

--- lib/core/pages/pages.dart
+++ lib/core/pages/pages.dart
@@ -1,4 +1,6 @@
+import 'package:doctor_app/features/auth/presentations/pages/identification_page.dart';
 import 'package:doctor_app/features/auth/presentations/pages/login_page.dart';
+import 'package:doctor_app/features/main/main_page.dart';
 import 'package:doctor_app/features/onboard/bindings.dart';
 import 'package:doctor_app/features/onboard/pages/languages_page.dart';
 import 'package:doctor_app/features/onboard/pages/onboard_page.dart';
@@ -8,18 +10,43 @@ import '../../features/auth/bindins.dart';
 import 'routes.dart';
 
 class AppPages {
-  static const initial = Routes.login; // first page when app starts
+  static const initial = Routes.login;
 
   static final pages = [
     GetPage(
       name: Routes.login,
       page: () => LoginPage(),
       binding: AuthBindings(),
+      transition: Transition.native,
+      transitionDuration: const Duration(milliseconds: 500),
     ),
     GetPage(
       name: Routes.onboard,
       page: () => OnboardPage(),
       binding: OnBoardBindings(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 400),
+    ),
+    GetPage(
+      name: Routes.lang,
+      page: () => LanguagesPage(),
+      binding: OnBoardBindings(),
+      transition: Transition.downToUp,
+      transitionDuration: const Duration(milliseconds: 400),
+    ),
+    GetPage(
+      name: Routes.main,
+      page: () => MainPage(),
+      transition: Transition.zoom,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+
+    GetPage(
+      name: Routes.identification,
+      page: () => IdentificationPage(),
+      binding: AuthBindings(),
+      transition: Transition.leftToRight,
+      transitionDuration: const Duration(milliseconds: 400),
     ),
   ];
 }
--- lib/core/pages/routes.dart
+++ lib/core/pages/routes.dart
@@ -2,4 +2,6 @@ class Routes {
   static const String login = '/login';
   static const String onboard = '/onboard';
   static const String lang = '/lang';
+  static const String main = '/main';
+  static const String identification = '/identification';
 }
