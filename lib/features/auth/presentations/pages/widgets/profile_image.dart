5e8f4906ca6949e2d007c0781f5517c8317e5556
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-23 12:29:51 +0500

    feat: profile image was created

---
+++ lib/features/auth/presentations/pages/widgets/profile_image.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:doctor_app/core/utils/asset_finder.dart';
+import 'package:doctor_app/core/widgets/circle.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfileImage extends StatefulWidget {
+  const ProfileImage({super.key});
+
+  @override
+  State<ProfileImage> createState() => _ProfileImageState();
+}
+
+class _ProfileImageState extends State<ProfileImage> {
+  final ImagePicker _picker = ImagePicker();
+  File? _imageFile;
+
+  Future<void> _pickImage() async {
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _pickImage,
+      child: _imageFile != null
+          ? Container(
+              alignment: Alignment.center,
+              child: Circle(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.file(_imageFile!, fit: BoxFit.cover),
+                ),
+              ),
+            )
+          : Container(
+              alignment: Alignment.center,
+              child: SvgPicture.asset(AssetFinder.icon('avatar'), height: 80),
+            ),
+    );
+  }
+}
