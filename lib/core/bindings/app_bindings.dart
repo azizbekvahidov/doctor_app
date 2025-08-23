8335b0683d279bce718849f048c5e466b3daea5a
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-23 15:59:34 +0500

    feat: AuthController was added

--- lib/core/bindings/app_bindings.dart
+++ lib/core/bindings/app_bindings.dart
@@ -1,3 +1,6 @@
+import 'package:doctor_app/core/services/dio_service.dart';
+import 'package:doctor_app/features/auth/data/repository/auth_repository_iml.dart';
+import 'package:doctor_app/features/auth/presentations/controller/auth_controller.dart';
 import 'package:doctor_app/features/onboard/controller/onboard_controller.dart';
 import 'package:get/get.dart';
 
@@ -5,5 +8,9 @@ class AppBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => OnboardController());
+    Get.lazyPut(
+      () => AuthController(AuthRepositoryIml(DioService().dio)),
+      fenix: true,
+    );
   }
 }
