0054dd691f124d521d3f53563b31ec21f7df5fa8
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-23 12:30:55 +0500

    ref: login method was updated. if there is error, return null

--- lib/features/auth/data/repository/auth_repository_iml.dart
+++ lib/features/auth/data/repository/auth_repository_iml.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:doctor_app/core/constants/api_constants.dart';
+import 'package:doctor_app/core/utils/log_helper.dart';
 import 'package:doctor_app/features/auth/domain/models/login_data.dart';
 import 'package:doctor_app/features/auth/domain/repository/auth_repository.dart';
 
@@ -10,7 +11,7 @@ class AuthRepositoryIml extends AuthRepository {
   @override
   Future<LoginData?> login(pinfl) async {
     try {
-      LoginData loginData = LoginData();
+      LoginData? loginData = LoginData();
       final Response response = await dio.post(
         ApiConstants.login,
         data: {'pinfl': pinfl},
@@ -19,7 +20,11 @@ class AuthRepositoryIml extends AuthRepository {
         loginData = LoginData.fromJson(response.data['data']);
       }
       return loginData;
+    } on DioException catch (e) {
+      LogHelper.error("Failed Login: ${e.toString()}");
+      return null;
     } catch (e) {
+      LogHelper.error("Failed Login: ${e.toString()}");
       return null;
     }
   }
