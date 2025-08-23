cf91d0e4c4a1c3e7436c1eea5425148ec9761188
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-23 12:27:11 +0500

    feat: added new endpoint

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,24 @@
 class ApiConstants {
   static const String mainUrl = "https://doctor-api.smartmobile.uz";
+  static const apiEgov = "/api/egov";
+  static const apiDoctor = "/api/doctor";
+  static const apiList = '/api/list';
 
   // Auth
-  static const String login = "/api/egov/auth";
-  static const String register = "/api/doctor/register";
-  static const String token = "/api/egov/get-token";
-  static const String logout = "/api/egov/logout";
+  static const String login = "$apiEgov/auth";
+  static const String register = "$apiDoctor/register";
+  static const String token = "$apiEgov/get-token";
+  static const String logout = "$apiEgov/logout";
+
+  // Profile
+  static const String avatar = "$apiDoctor/file/avatar";
+  static const String doc = "$apiDoctor/file";
+
+  // Work Schedule
+  static const String workSchedule = "$apiDoctor/schedule";
+
+  // Clinic
+  static const String clinic = "$apiList/clinic";
+  static const String region = "$apiList/region";
+  static const String degree = "$apiList/degree";
 }
