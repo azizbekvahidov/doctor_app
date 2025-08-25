556fa0ea09b2d3e53e1554aff6ec4fafde9588a5
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-25 23:57:00 +0500

    feat: register screen controller was connected

--- lib/core/bindings/app_bindings.dart
+++ lib/core/bindings/app_bindings.dart
@@ -1,6 +1,7 @@
 import 'package:doctor_app/core/services/dio_service.dart';
 import 'package:doctor_app/features/auth/data/repository/auth_repository_iml.dart';
 import 'package:doctor_app/features/auth/presentations/controller/auth_controller.dart';
+import 'package:doctor_app/features/auth/presentations/controller/register_screen_controller.dart';
 import 'package:doctor_app/features/onboard/controller/onboard_controller.dart';
 import 'package:get/get.dart';
 
@@ -12,5 +13,6 @@ class AppBindings extends Bindings {
       () => AuthController(AuthRepositoryIml(DioService().dio)),
       fenix: true,
     );
+    Get.lazyPut(() => RegisterScreenController(), fenix: true);
   }
 }
