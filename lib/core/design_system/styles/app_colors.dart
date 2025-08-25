ed45dba73cc182feb96a87acc4552af011c3d93f
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-25 23:58:51 +0500

    feat: new colors

--- lib/core/design_system/styles/app_colors.dart
+++ lib/core/design_system/styles/app_colors.dart
@@ -4,5 +4,6 @@ class AppColors {
   static const Color primary = Color(0xFF3A00E5),
       grey = Color(0xFFAFB1B6),
       red = Color(0xFFFF0000),
-      lightGray = Color(0xFFD9D9D9);
+      lightGray = Color(0xFFD9D9D9),
+      white = Colors.white;
 }
