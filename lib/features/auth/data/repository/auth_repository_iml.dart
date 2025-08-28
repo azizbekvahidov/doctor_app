bf0f401fa9688e47082a569a232d3a56bdc1919b
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-08-28 23:31:40 +0500

    feat: authdata updated

--- lib/features/auth/data/repository/auth_repository_iml.dart
+++ lib/features/auth/data/repository/auth_repository_iml.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:doctor_app/core/constants/api_constants.dart';
+import 'package:doctor_app/core/services/secure_storage_service.dart';
 import 'package:doctor_app/core/utils/log_helper.dart';
-import 'package:doctor_app/features/auth/domain/models/login_data.dart';
+import 'package:doctor_app/features/auth/domain/models/auth_data.dart';
 import 'package:doctor_app/features/auth/domain/repository/auth_repository.dart';
 
 class AuthRepositoryIml extends AuthRepository {
@@ -9,15 +10,15 @@ class AuthRepositoryIml extends AuthRepository {
   AuthRepositoryIml(this.dio);
 
   @override
-  Future<LoginData?> login(pinfl) async {
+  Future<AuthData?> login(pinfl) async {
     try {
-      LoginData? loginData = LoginData();
+      AuthData? loginData = AuthData();
       final Response response = await dio.post(
         ApiConstants.login,
         data: {'pinfl': pinfl},
       );
       if (response.statusCode == 200) {
-        loginData = LoginData.fromJson(response.data['data']);
+        loginData = AuthData.fromJson(response.data['data']);
       }
       return loginData;
     } on DioException catch (e) {
@@ -35,7 +36,25 @@ class AuthRepositoryIml extends AuthRepository {
   }
 
   @override
-  Future<void> register() async {
-    await dio.post(ApiConstants.login);
+  Future<bool> register(personalData) async {
+    final storeage = SecureStorageService();
+    String? token = await storeage.getToken();
+
+    try {
+      final Response response = await dio.post(
+        ApiConstants.register,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+        data: personalData,
+      );
+      return response.statusCode == 200;
+    } on DioException catch (e) {
+      // LogHelper.info(e.requestOptions.headers.toString());
+      // LogHelper.info(e.response!.statusCode!.toString());
+      // // LogHelper.error(e.requestOptions.uri.toString());
+      LogHelper.error(e.response!.data.toString());
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 }
