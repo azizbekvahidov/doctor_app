e67ccec2e33cba28ed3e6bc76eba37b1ee00d251
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-09-10 22:54:23 +0500

    feat: header was updated

--- lib/features/main/presentation/widgets/header.dart
+++ lib/features/main/presentation/widgets/header.dart
@@ -1,19 +1,35 @@
+import 'package:doctor_app/core/enums/contents.dart';
 import 'package:doctor_app/core/utils/asset_finder.dart';
 import 'package:doctor_app/core/design_system/widgets/circle.dart';
 import 'package:doctor_app/core/design_system/styles/app_colors.dart';
 import 'package:doctor_app/core/design_system/styles/text_styles.dart';
 import 'package:doctor_app/features/auth/presentations/controller/auth_controller.dart';
+import 'package:doctor_app/features/main/presentation/controller/main_page_controller.dart';
+import 'package:doctor_app/features/onboard/controller/onboard_controller.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-import '../../onboard/controller/onboard_controller.dart';
-
-class Header extends StatelessWidget {
+class Header extends StatefulWidget {
   Header({super.key});
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
   final OnboardController onboardController = Get.find<OnboardController>();
+
   final AuthController authController = Get.find<AuthController>();
+
+  final MainPageController mainPageController = Get.find<MainPageController>();
+
+  @override
+  void initState() {
+    onboardController.getUser();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +39,7 @@ class Header extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Obx(() {
+            if (onboardController.loadingUser.value) return SizedBox.shrink();
             return GestureDetector(
               onTap: () => authController.logout(),
               child: Row(
@@ -50,19 +67,24 @@ class Header extends StatelessWidget {
             );
           }),
           Spacer(),
-          SizedBox(
+          Container(
+            decoration: BoxDecoration(shape: BoxShape.circle),
             height: 65,
             child: Stack(
               alignment: Alignment.center,
               children: [
-                Circle(
-                  bgColor: AppColors.lightGray,
-                  border: Border.all(color: Colors.transparent),
-                  width: 50,
-                  height: 50,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: SvgPicture.asset(AssetFinder.icon('notification')),
+                InkWell(
+                  onTap: () => mainPageController.setContent(Contents.archive),
+                  borderRadius: BorderRadius.circular(50),
+                  child: Circle(
+                    bgColor: AppColors.lightGray,
+                    border: Border.all(color: Colors.transparent),
+                    width: 50,
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: SvgPicture.asset(AssetFinder.icon('notification')),
+                    ),
                   ),
                 ),
                 Positioned(
