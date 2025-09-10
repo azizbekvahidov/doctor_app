a5b88724671ac4fe577b6ba7ee7dac0abb3f02a2
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-09-10 22:53:58 +0500

    feat: bindings were updated

--- lib/features/onboard/bindings.dart
+++ lib/features/onboard/bindings.dart
@@ -1,10 +1,14 @@
+import 'package:doctor_app/features/auth/presentations/controller/auth_controller.dart';
 import 'package:get/get.dart';
 
+import '../../core/services/dio_service.dart';
+import '../auth/data/repository/auth_repository_iml.dart';
 import 'controller/onboard_controller.dart';
 
 class OnBoardBindings extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(() => OnboardController(), fenix: true);
+    Get.lazyPut(() => OnboardController());
+    Get.lazyPut(() => AuthController(AuthRepositoryIml(DioService().dio)));
   }
 }
