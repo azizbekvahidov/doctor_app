bda3adf10cd6486464000c057e68b1f02c26c591
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-09-10 22:50:50 +0500

    feat: new color was added

--- lib/core/design_system/styles/app_colors.dart
+++ lib/core/design_system/styles/app_colors.dart
@@ -5,5 +5,8 @@ class AppColors {
       grey = Color(0xFFAFB1B6),
       red = Color(0xFFFF0000),
       lightGray = Color(0xFFD9D9D9),
-      white = Colors.white;
+      white = Colors.white,
+      subTitleDark = Color(0xFF61646B),
+      subTitleLight = Color(0xFFAFB1B6),
+      surface = Color(0xFFFAFAFA);
 }
