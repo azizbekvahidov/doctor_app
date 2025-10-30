f39a017605f006a814453a4115874b9ed4dfbc65
Akhmadjon Akbarov <akhmadjonakbarov@gmail.com>
2025-10-31 02:31:43 +0500

    feat: add refresh indicator functionality to get data

--- lib/features/main/presentation/contents/home_content.dart
+++ lib/features/main/presentation/contents/home_content.dart
@@ -1,59 +1,57 @@
 import 'package:doctor_app/core/navigation/routes.dart';
 import 'package:doctor_app/features/main/presentation/widgets/header.dart';
+import 'package:doctor_app/features/main/presentation/widgets/new_issue_list.dart';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
+import '../../../shared/controllers/issue_controller.dart';
 import '../widgets/active_card.dart';
 import '../widgets/announcement_list.dart';
-import '../widgets/new_card.dart';
 
 class HomeContent extends StatelessWidget {
   const HomeContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Header(),
-          Padding(
-            padding: const EdgeInsets.all(8.0).copyWith(top: 15),
-            child: Column(
-              children: [
-                AnnouncementList(
-                  title: "new_ann".tr,
-                  height: 260,
-                  child: ListView.separated(
-                    itemCount: 15,
-                    separatorBuilder: (_, __) => const SizedBox(width: 10),
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (_, __) => NewCard(
-                      onClick: () {
-                        Get.toNamed(Routes.detail);
-                      },
+    final issueController = Get.find<IssueController>();
+
+    return RefreshIndicator(
+      color: Colors.black,
+      onRefresh: () async {
+        // 👇 refresh all needed sections
+        await issueController.getInitialIssues();
+      },
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(), // important!
+        child: Column(
+          children: [
+            Header(),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(top: 15),
+              child: Column(
+                children: [
+                  const NewIssueList(),
+                  const SizedBox(height: 30),
+                  AnnouncementList(
+                    title: "active_ann".tr,
+                    height: 175,
+                    child: ListView.separated(
+                      itemCount: 15,
+                      separatorBuilder: (_, __) => const SizedBox(width: 10),
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (_, __) => ActiveCard(
+                        onClick: () {
+                          Get.toNamed(Routes.detail);
+                        },
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 30),
-                AnnouncementList(
-                  title: "active_ann".tr,
-                  height: 175,
-                  child: ListView.separated(
-                    itemCount: 15,
-                    separatorBuilder: (_, __) => const SizedBox(width: 10),
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (_, __) => ActiveCard(
-                      onClick: () {
-                        Get.toNamed(Routes.detail);
-                      },
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
